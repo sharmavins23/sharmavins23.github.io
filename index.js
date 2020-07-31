b8fbb137fce9d98fcf116532212566fbbcbd9ab3
Vins <sharmavins23@gmail.com>
2020-07-31 09:59:22 -0400

    🎉 testing bundling with gh static pages

---
+++ index.js
@@ -0,0 +1,18 @@
+// Imports
+import React from "react";
+import ReactDOM from "react-dom";
+
+// Main container
+function App() {
+    // Contains all individual components and layout
+
+    // Render
+    return (
+        <div>
+            <h1>Testing the webpacking out!</h1>
+        </div>
+    );
+}
+
+// Render with the DOM
+ReactDOM.render(<App />, document.getElementById("app"));
