b2cc5ed9571fe0403740fab2bd3a1e9b58313f26
Vins <sharmavins23@gmail.com>
2020-07-31 20:32:53 -0400

    🔥 updated header with filler text

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 // Imports
 import React from "react";
 import { Typography, Divider } from "antd";
-const { Title } = Typography;
+const { Title, Text } = Typography;
 // Styles
 import styles from "../src/stylesheet.module.css";
 import {} from "antd/dist/antd.css";
@@ -11,10 +11,27 @@ function Header() {
     // Render
     return (
         <div>
+            {/* Name */}
             <Divider />
             <div class={styles.header_name}>
                 <Title>Vins Sharma</Title>
             </div>
+
+            {/* Info */}
+            <Divider />
+            <table class={styles.header_table}>
+                <tr>
+                    {/* Left side */}
+                    <td></td>
+
+                    {/* Right side */}
+                    <td class={styles.header_table_cell_right}>
+                        <Title level={4} style={{ color: "DarkRed" }}>
+                            Contact Information
+                        </Title>
+                    </td>
+                </tr>
+            </table>
             <Divider />
         </div>
     );
